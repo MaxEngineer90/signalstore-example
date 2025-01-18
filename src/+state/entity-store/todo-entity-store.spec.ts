3fbc88309da315c927a854027879039f09cb52c8
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2025-01-18 23:26:26 +0100

    edit spec tests, and small refactorings in todo-entity-store.ts

--- src/+state/entity-store/todo-entity-store.spec.ts
+++ src/+state/entity-store/todo-entity-store.spec.ts
@@ -1,46 +1,72 @@
-import {TodosEntityStore} from './todo-entity-store';
-import {TestBed} from '@angular/core/testing';
-import {TodoBackendService} from '../../service/todo-backend-service';
-import {of} from 'rxjs';
-import {provideHttpClientTesting} from '@angular/common/http/testing';
-import {MockService} from 'ng-mocks';
-import {provideHttpClient} from '@angular/common/http';
-import {instance, mock, when} from 'ts-mockito';
-
-export const mockTodos = [
-  {id: 1, title: 'Test Todo 1', completed: false},
-  {id: 2, title: 'Test Todo 2', completed: true},
-];
-
+import { TodosEntityStore } from './todo-entity-store';
+import { TestBed } from '@angular/core/testing';
+import { TodoBackendService } from '../../service/todo-backend-service';
+import { MockService } from 'ng-mocks';
+import { EMPTY, of } from 'rxjs';
+import { TodoDto } from '../../models/todo-dto';
 
 describe('TodosEntityStore', () => {
-  let store = TestBed.inject(TodosEntityStore)
-  const todoBackendService: TodoBackendService = mock(
-    TodoBackendService
-  );
+  const mockTodos = [
+    { id: 1, title: 'Test Todo 1', completed: false },
+    { id: 2, title: 'Test Todo 2', completed: true },
+  ];
+  let todoBackendService: TodoBackendService;
 
   beforeEach(() => {
-    when(todoBackendService.getTodos()).thenCall(() => {
-      return of(mockTodos);
-    });
-
-
-    // TestBed konfigurieren
+    todoBackendService = MockService(TodoBackendService);
+    jest.spyOn(todoBackendService, 'getTodos').mockReturnValue(of(mockTodos));
     TestBed.configureTestingModule({
       providers: [
-        TodosEntityStore,
-        {provide: TodoBackendService, useValue: todoBackendService},
-        provideHttpClientTesting(),
+        { provide: TodoBackendService, useValue: todoBackendService },
       ],
     });
   });
 
   it('should load todos successfully', () => {
+    const store = TestBed.inject(TodosEntityStore);
+    expect(store.sortedTodos()).toEqual(mockTodos);
+  });
 
-    // Überprüfen, ob der Store die geladenen Todos gespeichert hat
-    expect(store.sortedTodos()).toEqual([
-      {id: 1, title: 'Test Todo 1', completed: false},
-      {id: 2, title: 'Test Todo 2', completed: true},
-    ]);
+  it('should create a new todo', () => {
+    const newTodo: TodoDto = {
+      id: 3,
+      title: 'Test Todo 1',
+      completed: false,
+    };
+    jest.spyOn(todoBackendService, 'createTodo').mockImplementation(() => {
+      mockTodos.push(newTodo);
+      return of(newTodo);
+    });
+
+    const store = TestBed.inject(TodosEntityStore);
+    store.createTodo(newTodo);
+    expect(todoBackendService.createTodo).toHaveBeenCalledWith(newTodo);
+    store.selectTodoById(newTodo.id);
+    expect(store.selectedEntity()).toEqual(expect.objectContaining(newTodo));
+    expect(store.todoCount()).toBe(3);
+  });
+
+  it('should update an existing todo', () => {
+    const updatedTodo = { id: 1, title: 'Updated Todo', completed: false };
+    const store = TestBed.inject(TodosEntityStore);
+    jest
+      .spyOn(todoBackendService, 'updateTodo')
+      .mockImplementation((todo) => of({ ...todo, completed: todo.completed }));
+    store.updateTodo(updatedTodo);
+    expect(todoBackendService.updateTodo).toHaveBeenCalledWith(updatedTodo);
+    const storedTodo = store.entityMap()[1];
+    expect(storedTodo.completed).toBe(true);
+  });
+
+  it('should delete a todo', () => {
+    const store = TestBed.inject(TodosEntityStore);
+    jest
+      .spyOn(todoBackendService, 'deleteTodo')
+      .mockImplementation((id) => EMPTY);
+    expect(store.entityMap()[1]).toEqual(mockTodos[0]);
+    store.deleteTodo(1);
+    expect(todoBackendService.deleteTodo).toHaveBeenCalledWith(1);
+
+    console.log(store.entityMap());
   });
 });
